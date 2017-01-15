71f51da1a0b50c62e77114c27810462ca40f01fc
rebel-l <l.gaubisch@googlemail.com>
2017-01-15 00:11:08 +0000

    Generated code by SwaggerHub

---
+++ src/go/internal.go
@@ -0,0 +1,25 @@
+package 
+
+import (
+	"net/http"
+)
+
+type Internal struct {
+
+}
+
+func DeleteSession(w http.ResponseWriter, r *http.Request) {
+		w.Header().Set("Content-Type", "application/json; charset=UTF-8")
+		w.WriteHeader(http.StatusOK)
+}
+
+func LoadSession(w http.ResponseWriter, r *http.Request) {
+		w.Header().Set("Content-Type", "application/json; charset=UTF-8")
+		w.WriteHeader(http.StatusOK)
+}
+
+func UpdateSession(w http.ResponseWriter, r *http.Request) {
+		w.Header().Set("Content-Type", "application/json; charset=UTF-8")
+		w.WriteHeader(http.StatusOK)
+}
+
