544c67d3d108d829f1a8b13172510fdd0d0a8967
Rebel-L <dj@rebel-l.net>
2017-09-23 16:48:09 +0200

    #5: setup the basic travis ci

--- try/main.go
+++ try/main.go
@@ -1,14 +1,14 @@
 package main
 
 import (
-	"gopkg.in/redis.v5"
+	//"gopkg.in/redis.v5"
 	//"fmt"
 )
 
 func main()  {
-	println("Hello");
-	var test  = redis.Nil;
-	if (test != redis.Nil) {
-		println("Toll");
-	}
+	//println("Hello");
+	//var test  = redis.Nil;
+	//if (test != redis.Nil) {
+	//	println("Toll");
+	//}
 }
