8b6768793568395fd80a6dac55400cf605654ee9
Rebel-L <dj@rebel-l.net>
2017-09-23 20:13:24 +0200

    #6: made the existing code compatible with gometalinter

--- hello.go
+++ hello.go
@@ -1,9 +1,11 @@
-package SessionService
+package main
 
 import "fmt"
 
 func main(){
+	var a = multiply(6,7)
 	fmt.Printf("Hello World\n")
+	fmt.Printf("6 x 7 is %d\n", a)
 }
 
 func multiply(a int, b int) int {
--- hello_test.go
+++ hello_test.go
@@ -1,4 +1,4 @@
-package SessionService
+package main
 
 import "testing"
 
