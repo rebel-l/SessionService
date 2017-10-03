6e916fff59c9fbb0c53ab31d3162f9aa036a1b06
Rebel-L <dj@rebel-l.net>
2017-10-04 00:42:58 +0200

    #9: implemented a first ping to redis container

--- main.go
+++ main.go
@@ -2,14 +2,31 @@ package main
 
 import (
 	"fmt"
+	"github.com/go-redis/redis"
 )
 
 func main(){
 	var a = multiply(6,7)
 	fmt.Printf("Hello World\n")
 	fmt.Printf("6 x 7 is %d\n", a)
+	redisPing()
 }
 
 func multiply(a int, b int) int {
 	return a * b
+}
+
+func getRedisClient() *redis.Client {
+	client := redis.NewClient(&redis.Options{
+		Addr: "redis:6379",
+		Password: "",
+		DB: 0,
+	})
+
+	return client
+}
+
+func redisPing() {
+	pong, err := getRedisClient().Ping().Result()
+	fmt.Println(pong, err)
 }
