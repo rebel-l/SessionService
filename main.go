114ab65b3b8770694b27da5f5ecdfc395b5bb50a
Rebel-L <dj@rebel-l.net>
2017-10-04 14:23:20 +0200

    #9: implemented set & get value to redis

--- main.go
+++ main.go
@@ -10,6 +10,10 @@ func main(){
 	fmt.Printf("Hello World\n")
 	fmt.Printf("6 x 7 is %d\n", a)
 	redisPing()
+	setEntry("name", "Lars")
+	setEntry("age", "29")
+	fmt.Printf("Hello %s!\n", getEntry("name"))
+	fmt.Printf("You are %s years old!\n", getEntry("age"))
 }
 
 func multiply(a int, b int) int {
@@ -29,4 +33,20 @@ func getRedisClient() *redis.Client {
 func redisPing() {
 	pong, err := getRedisClient().Ping().Result()
 	fmt.Println(pong, err)
+}
+
+func setEntry(key string, value string) {
+	err := getRedisClient().Set(key, value, 0)
+	if err.Err() != nil {
+		panic(err)
+	}
+}
+
+func getEntry(key string) string {
+	result, err := getRedisClient().Get(key).Result()
+	if err != nil {
+		panic(err)
+	}
+
+	return result
 }
