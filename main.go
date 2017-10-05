5bc7592fc726edf59189b73e48821b73c8d0ec6d
Rebel-L <dj@rebel-l.net>
2017-10-05 13:51:17 +0200

    #10: implemented the static docs endpoint - changed port of service to 4000 - fixed the metalinter warnings and removed the exclude entry - added serveStatic() method started by flag -server

--- main.go
+++ main.go
@@ -1,16 +1,20 @@
 package main
 
 import (
-	"fmt"
 	"flag"
+	"fmt"
 	"github.com/go-redis/redis"
+	"log"
+	"net/http"
 )
 
 func main() {
 	// parse the flags
 	runRedis := flag.Bool("redis", false, "execute redis operations")
+	runServer := flag.Bool("server", false, "execute server listening")
 	flag.Parse()
 	fmt.Printf("Run Redis: %t\n", *runRedis)
+	fmt.Printf("Run Server: %t\n", *runServer)
 
 	// print some texts
 	a := multiply(6,7)
@@ -18,12 +22,27 @@ func main() {
 	fmt.Printf("6 x 7 is %d\n", a)
 
 	// do some redis operations if redis flag is set
-	if *runRedis == true {
+	if *runRedis {
 		redisPing()
 		setEntry("name", "Lars")
 		setEntry("age", "29")
 		fmt.Printf("Hello %s!\n", getEntry("name"))
 		fmt.Printf("You are %s years old!\n", getEntry("age"))
+	}
+
+	// start to serve
+	if *runServer {
+		serveStatic()
+	}
+}
+
+func serveStatic() {
+	fs := http.FileServer(http.Dir("docs"))
+	http.Handle("/docs/", http.StripPrefix("/docs/", fs))
+	log.Printf("Listening ...")
+	err := http.ListenAndServe(":4000", nil)
+	if  err != nil {
+		panic(err)
 	}
 }
 
