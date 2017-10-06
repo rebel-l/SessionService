47dabe375c23ab3edf1455fe31e30c71e59093a4
Rebel-L <dj@rebel-l.net>
2017-10-07 01:06:05 +0200

    #22: implemented the response.Ping model including tests and examples

--- main.go
+++ main.go
@@ -4,42 +4,76 @@ import (
 	"flag"
 	"fmt"
 	"github.com/go-redis/redis"
+	"github.com/rebel-l/sessionservice/src/response"
 	"log"
 	"net/http"
+	"encoding/json"
 )
 
 func main() {
+	fmt.Println("")
+
+	// response.PingSummary
+	fmt.Println("resonse.PingSummary ...")
+	ps := response.NewPingSummary()
+	fmt.Printf("\tStruct (Before): %#v\n", ps)
+	ps.TurnServiceOnline()
+	fmt.Printf("\tService Online: %s\n", ps.Service)
+	ps.TurnStorageOnline()
+	fmt.Printf("\tStorage Online: %s\n", ps.Storage)
+	fmt.Printf("\tStruct (After): %#v\n", ps)
+	res, _ := json.Marshal(ps)
+	fmt.Printf("\tJSON Output %s\n", string(res))
+	fmt.Println("")
+
+	// response.Ping
+	fmt.Println("response.Ping ...")
+	p := response.NewPing()
+	fmt.Printf("\tPing Struct (Before): %#v\n", p)
+	p.Summary = ps
+	p.Notify()
+	fmt.Printf("\tPing Struct (After): %#v\n", p)
+	fmt.Println("")
+
 	// parse the flags
+	fmt.Println("Parse flags ...")
 	runRedis := flag.Bool("redis", false, "execute redis operations")
 	runServer := flag.Bool("server", false, "execute server listening")
 	flag.Parse()
-	fmt.Printf("Run Redis: %t\n", *runRedis)
-	fmt.Printf("Run Server: %t\n", *runServer)
+	fmt.Printf("\tRun Redis: %t\n", *runRedis)
+	fmt.Printf("\tRun Server: %t\n", *runServer)
+	fmt.Println("")
 
 	// print some texts
+	fmt.Println("Print some examples ...")
 	a := multiply(6,7)
-	fmt.Printf("Hello World\n")
-	fmt.Printf("6 x 7 is %d\n", a)
+	fmt.Printf("\tHello World\n")
+	fmt.Printf("\t6 x 7 is %d\n", a)
+	fmt.Println("")
 
 	// do some redis operations if redis flag is set
 	if *runRedis {
+		fmt.Println("Redis ...")
 		redisPing()
 		setEntry("name", "Lars")
 		setEntry("age", "29")
-		fmt.Printf("Hello %s!\n", getEntry("name"))
-		fmt.Printf("You are %s years old!\n", getEntry("age"))
+		fmt.Printf("\tHello %s!\n", getEntry("name"))
+		fmt.Printf("\tYou are %s years old!\n", getEntry("age"))
+		fmt.Println("")
 	}
 
 	// start to serve
 	if *runServer {
+		fmt.Println("Static Server ...")
 		serveStatic()
+		fmt.Println("")
 	}
 }
 
 func serveStatic() {
 	fs := http.FileServer(http.Dir("docs"))
 	http.Handle("/docs/", http.StripPrefix("/docs/", fs))
-	log.Printf("Listening ...")
+	log.Printf("\tListening ...")
 	err := http.ListenAndServe(":4000", nil)
 	if  err != nil {
 		panic(err)
---
+++ src/response/ping_test.go
@@ -0,0 +1,78 @@
+package response
+
+import (
+	"testing"
+	"github.com/stretchr/testify/assert"
+	//"encoding/json"
+	"encoding/json"
+)
+
+func TestPingDefaultValues(t *testing.T) {
+	p := NewPing()
+	assert.Equal(t, FAILURE, p.Success, "Success should return an " + FAILURE + " string as default")
+	assert.Equal(t, new(PingSummary), p.Summary, "Summary should return an empty PingSummary as default")
+}
+
+func TestPingNotify(t *testing.T) {
+	cases := pingDataProviderNotify()
+	for _, c := range cases {
+		c.actual.Notify()
+		msg := "Notify should set Success to " + c.expected
+		msg += " if PingSummary.Service is " + c.actual.Summary.Service
+		msg += " and PingSummary.Storage is " + c.actual.Summary.Storage
+		assert.Equal(t, c.expected, c.actual.Success,  msg)
+	}
+}
+
+func pingDataProviderNotify() []pingData {
+	// only Service has PONG
+	psServiceOnly := NewPingSummary()
+	psServiceOnly.TurnServiceOnline()
+
+	// only Storage has PONG
+	psStorageOnly := NewPingSummary()
+	psStorageOnly.TurnStorageOnline()
+
+	// both Service and Storage has PONG
+	psBoth := NewPingSummary()
+	psBoth.TurnServiceOnline()
+	psBoth.TurnStorageOnline()
+
+	return []pingData{
+		{FAILURE, NewPing()},
+		{FAILURE, newPing(psServiceOnly)},
+		{FAILURE, newPing(psStorageOnly)},
+		{SUCCESS, newPing(psBoth)},
+	}
+}
+
+func TestPingJsonEncoding(t *testing.T)  {
+	cases := pingDataProviderJson()
+	for _, c := range cases {
+		assert.Equal(t, c.expected, pingJsonEncode(c.actual), "The JSON encoded struct is not matching")
+	}
+}
+
+func pingDataProviderJson() []pingData {
+	ps := NewPingSummary()
+	ps.TurnServiceOnline()
+	ps.TurnStorageOnline()
+
+	p := newPing(ps)
+	p.Notify()
+
+	return []pingData {
+		{"{\"Success\":\"FAIL\",\"Summary\":{\"Service\":\"\",\"Storage\":\"\"}}", NewPing()},
+		{"{\"Success\":\"OK\",\"Summary\":{\"Service\":\"PONG\",\"Storage\":\"PONG\"}}", p},
+	}
+}
+
+func pingJsonEncode(ps *Ping) string {
+	res, _ := json.Marshal(ps)
+	return string(res)
+}
+
+type pingData struct {
+	expected string
+	actual *Ping
+}
