db8a26f388e98978dc52a519bffe7f36414d65a6
Rebel-L <dj@rebel-l.net>
2017-10-07 19:42:32 +0200

    #19: build in the notification and check logic. redis checks still missing, added a TODO

--- src/endpoint/ping.go
+++ src/endpoint/ping.go
@@ -12,6 +12,7 @@ const contentType = "application/json"
 
 type Ping struct {
 	response *response.Ping
+	observer []response.Observer
 }
 
 func InitPing() {
@@ -19,6 +20,7 @@ func InitPing() {
 
 	p := new(Ping)
 	p.response = response.NewPing()
+	p.observer = append(p.observer, p.response)
 	http.HandleFunc("/ping/", p.handler)
 
 	log.Debug("Ping endpoint: initialized!")
@@ -27,8 +29,37 @@ func InitPing() {
 func (p *Ping) handler(res http.ResponseWriter, req *http.Request) {
 	log.Debug("Ping: request received ...")
 
-	res.Header().Set(contentHeader, contentType)
-	json.NewEncoder(res).Encode(p.response)
+	// do the checks
+	p.checkService()
+	p.checkStorage()
+
+	// send response
+	p.send(res)
 
 	log.Debug("Ping: response send!")
+}
+
+func (p *Ping) notify()  {
+	for _, o := range p.observer {
+		o.Notify()
+	}
+}
+
+func (p *Ping) checkService() {
+	p.response.Summary.TurnServiceOnline()
+	p.notify()
+}
+
+func (p *Ping) checkStorage() {
+	// TODO: implement redis ping here ...
+	p.response.Summary.TurnStorageOnline()
+	p.notify()
+}
+
+func (p *Ping) send(res http.ResponseWriter)  {
+	res.Header().Set(contentHeader, contentType)
+	if p.response.Success != response.SUCCESS {
+		res.WriteHeader(http.StatusServiceUnavailable)
+	}
+	json.NewEncoder(res).Encode(p.response)
 }
