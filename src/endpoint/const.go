7a41d93c53ebd16aa81fa3234234c797e1cdf2df
Rebel-L <dj@rebel-l.net>
2017-11-19 18:50:17 +0100

    #27: implemented parsing update body

--- src/endpoint/const.go
+++ src/endpoint/const.go
@@ -1,5 +1,8 @@
 package endpoint
 
-const contentHeader = "Content-Type"
-const contentTypeJson = "application/json"
-const contentTypePlain = "text/plain; charset=utf-8"
+const (
+	contentHeader = "Content-Type"
+	contentTypeJson = "application/json"
+	contentTypePlain = "text/plain; charset=utf-8"
+	UUIDLENGTH = 36	// includes dashes: 8-4-4-4-12
+)
--- src/endpoint/session.go
+++ src/endpoint/session.go
@@ -2,13 +2,20 @@ package endpoint
 
 import (
 	"encoding/json"
+	"errors"
+	"fmt"
 	"github.com/go-redis/redis"
 	"github.com/gorilla/mux"
+	"github.com/pborman/uuid"
 	"github.com/rebel-l/sessionservice/src/authentication"
 	"github.com/rebel-l/sessionservice/src/request"
 	"github.com/rebel-l/sessionservice/src/response"
 	log "github.com/sirupsen/logrus"
 	"net/http"
+)
+
+const (
+	putBadRequestText = "A body needs to be send as JSON and it needs at least a data field"
 )
 
 // Session handles the session endpoints
@@ -26,30 +33,30 @@ func InitSession(redisClient *redis.Client, router *mux.Router, middleware *auth
 	s.middleware = middleware
 	router.Handle("/session/", s.handlerFactory(http.MethodPut)).Methods(http.MethodPut)
 
-	log.Debug("Ping endpoint: initialized!")
+	log.Debug("Session endpoint: initialized!")
 }
 
 func (s *Session) handlePut(res http.ResponseWriter, req *http.Request) {
-	log.Println("Executing session PUT")
+	log.Info("Executing session PUT")
 
 	// read request body
-	decoder := json.NewDecoder(req.Body)
-	defer req.Body.Close()
-	var data request.Update
-	err := decoder.Decode(&data)
+	requestBody, err := s.getRequestBody(req)
 	if err != nil {
-		log.Errorf("Unable to read request body: %s", err)
+		log.Errorf("Parsing request body failed: %s", err)
+		s.sendPlain(putBadRequestText, res, http.StatusBadRequest)
+		return
 	}
 
-	log.Debugf("Id to update: %s", data.Id)
-	for key, value := range data.Data {
+	// store session TODO: implement
+	log.Debugf("Id to update: %s", requestBody.Id)
+	for key, value := range requestBody.Data {
 		log.Debugf("%s: %s", key, value)
 	}
 
 	// write request
-	res.Header().Set(contentHeader, contentType)
+	res.Header().Set(contentHeader, contentTypeJson)
 	res.WriteHeader(http.StatusOK)
-	session := response.NewSession("",0)
+	session := response.NewSession(requestBody.Id,0)
 	err = json.NewEncoder(res).Encode(session)
 	if err != nil {
 		log.Errorf("Wasn't able to write body: %s", err)
@@ -69,4 +76,45 @@ func (s *Session) handlerFactory(method string) http.Handler {
 	}
 
 	return s.middleware.Middleware(http.HandlerFunc(handler))
+}
+
+func (s *Session) getRequestBody(req *http.Request) (body request.Update, err error) {
+	// read request body
+	decoder := json.NewDecoder(req.Body)
+	defer req.Body.Close()
+	err = decoder.Decode(&body)
+	if err != nil {
+		err = errors.New(fmt.Sprintf("Unable to read request body: %s", err))
+		return
+	}
+
+	err = s.validateRequestBody(&body)
+	return
+}
+
+func (s *Session) validateRequestBody(body *request.Update) error {
+	// Id must be uuid or empty string
+	if body.Id != "" {
+		body.Id = uuid.Parse(body.Id).String()
+		log.Debugf("UUID parsed: %s", body.Id)
+		if len(body.Id) != UUIDLENGTH {
+			return errors.New("request body validation failed ==> wrong UUID provided")
+		}
+	}
+
+	// data field must have entries
+	if len(body.Data) < 1 {
+		return errors.New("request body validation failed ==> no data field received")
+	}
+
+	return nil
+}
+
+func (s *Session) sendPlain(msg string, res http.ResponseWriter, code int) {
+	res.Header().Set(contentHeader, contentTypePlain)
+	res.WriteHeader(code)
+	i,_ := res.Write([]byte(msg))
+	if i < 1 {
+		log.Errorf("Wasn't able to write body: %d", i)
+	}
 }
