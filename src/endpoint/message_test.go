5dd222f3b6c33ff1770d68367639dfa484cd34c8
rebel-l <dj@rebel-l.net>
2017-12-27 12:27:15 +0100

    #27: added missing unittest for message struct

--- src/endpoint/message_test.go
+++ src/endpoint/message_test.go
@@ -1,6 +1,7 @@
 package endpoint
 
 import (
+	"errors"
 	"fmt"
 	"github.com/stretchr/testify/assert"
 	"net/http"
@@ -30,6 +31,36 @@ type WriteMock struct {
 	dataLen int
 }
 
+type PlainTestCase struct {
+	code int
+	msg string
+	mock *ResponseWriterMock
+}
+
+func dataProviderPlainTestCases() []PlainTestCase {
+	dataProvider := make([]PlainTestCase, 3)
+
+	// TestCase 1. Happy Path
+	dataProvider[0].code = 123
+	dataProvider[0].msg = "MyMessage"
+	wm1 := getWriterMock(len(dataProvider[0].msg), nil)
+	dataProvider[0].mock= getResponseWriterMock(wm1)
+
+	// TestCase 2. Error Unhappy Path
+	dataProvider[1].code = 333
+	dataProvider[1].msg = "Error"
+	wm2 := getWriterMock(len(dataProvider[1].msg), errors.New("Something went wrong"))
+	dataProvider[1].mock = getResponseWriterMock(wm2)
+
+	// TestCase 3. Data Length 0 Unhappy Path
+	dataProvider[2].code = 444
+	dataProvider[2].msg = "Data Length 0"
+	wm3 := getWriterMock(0, nil)
+	dataProvider[2].mock = getResponseWriterMock(wm3)
+
+	return dataProvider
+}
+
 func (rwm ResponseWriterMock) Write(data []byte) (int, error) {
 	rwm.WriteMock.counter++
 	rwm.WriteMock.data = string(data)
@@ -54,17 +85,47 @@ func TestMessageNew(t *testing.T) {
 	assert.Equal(t, fixture, msg.res, "Message has not the right response writer set")
 }
 
-func TestMessagePlainHappy(t *testing.T) {
-	fixture := "MyMessage"
-	mock := ResponseWriterMock{}
+func getResponseWriterMock(wm *WriteMock) *ResponseWriterMock {
+	mock := new(ResponseWriterMock)
 	mock.HeaderMock = new(HeaderMock)
 	mock.WriteHeaderMock = new(WriteHeaderMock)
-	wm := new(WriteMock)
-	wm.dataLen = len(fixture)
-	wm.err = nil
 	mock.WriteMock = wm
+	return mock
+}
+
+func getWriterMock(length int, err error) *WriteMock {
+	wm := new(WriteMock)
+	wm.dataLen = length
+	wm.err = err
+	return wm
+}
+
+func TestMessagePlainHappy(t *testing.T) {
+	for _, v := range dataProviderPlainTestCases() {
+		msg := NewMessage(v.mock)
+		msg.Plain(v.msg, v.code)
+
+		// Header
+		testErrMsg := fmt.Sprintf("Message has not set header for '%s: %s'", ContentHeader, ContentTypePlain)
+		assert.Equal(t, 1, v.mock.HeaderMock.counter, "Header not set")
+		assert.Equal(t, ContentTypePlain, v.mock.HeaderMock.header.Get(ContentHeader), testErrMsg)
+
+		// WriteHeader
+		assert.Equal(t, 1, v.mock.WriteHeaderMock.counter, "Header not written")
+		assert.Equal(t, v.code, v.mock.WriteHeaderMock.code, "Header code not written")
+
+		// Write
+		assert.Equal(t, 1, v.mock.WriteMock.counter, "Data not written")
+		assert.Equal(t, v.msg, v.mock.WriteMock.data, "Data not written")
+	}
+}
+
+func TestInternalServerErrorHappy(t *testing.T) {
+	fixture := "Internal Server Error"
+	wm := getWriterMock(len(fixture), nil)
+	mock := getResponseWriterMock(wm)
 	msg := NewMessage(mock)
-	msg.Plain(fixture, 123)
+	msg.InternalServerError(fixture)
 
 	// Header
 	testErrMsg := fmt.Sprintf("Message has not set header for '%s: %s'", ContentHeader, ContentTypePlain)
@@ -73,11 +134,35 @@ func TestMessagePlainHappy(t *testing.T) {
 
 	// WriteHeader
 	assert.Equal(t, 1, mock.WriteHeaderMock.counter, "Header not written")
-	assert.Equal(t, 123, mock.WriteHeaderMock.code, "Header code not written")
+	assert.Equal(t, http.StatusInternalServerError, mock.WriteHeaderMock.code, "Header code not written")
 
 	// Write
 	assert.Equal(t, 1, mock.WriteMock.counter, "Data not written")
 	assert.Equal(t, fixture, mock.WriteMock.data, "Data not written")
 }
 
-// TODO: write unhappy 1. writeMock returns error & 2. writeMock returns len < 1
+func TestSendJson(t *testing.T) {
+	data := make(map[string]string)
+	data["key1"] = "value 1"
+	data["key2"] = "value 2"
+
+	fixture := "{\"key1\":\"value 1\",\"key2\":\"value 2\"}\n"
+
+	wm := getWriterMock(len(fixture), nil)
+	mock := getResponseWriterMock(wm)
+	msg := NewMessage(mock)
+	msg.SendJson(data, 200)
+
+	// Header
+	testErrMsg := fmt.Sprintf("Message has not set header for '%s: %s'", ContentHeader, ContentTypePlain)
+	assert.Equal(t, 1, mock.HeaderMock.counter, "Header not set")
+	assert.Equal(t, ContentTypeJson, mock.HeaderMock.header.Get(ContentHeader), testErrMsg)
+
+	// WriteHeader
+	assert.Equal(t, 1, mock.WriteHeaderMock.counter, "Header not written")
+	assert.Equal(t, 200, mock.WriteHeaderMock.code, "Header code not written")
+
+	// Write
+	assert.Equal(t, 1, mock.WriteMock.counter, "Data not written")
+	assert.Equal(t, fixture, mock.WriteMock.data, "Data not written")
+}
