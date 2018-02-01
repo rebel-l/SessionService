05f7c26a454217cf19bc90f18aeb8d8f6c464962
rebel-l <dj@rebel-l.net>
2018-02-01 18:40:11 +0100

    #27: added more unit tests

--- src/endpoint/session/put_test.go
+++ src/endpoint/session/put_test.go
@@ -2,15 +2,17 @@ package session
 
 import (
 	"encoding/json"
+	"errors"
 	"github.com/rebel-l/sessionservice/src/authentication"
 	"github.com/rebel-l/sessionservice/src/configuration"
 	"github.com/rebel-l/sessionservice/src/response"
 	"github.com/rebel-l/sessionservice/src/storage"
 	"github.com/stretchr/testify/assert"
+	"net/http"
+	"strings"
 	"testing"
 	"time"
-	"errors"
-	"net/http"
+	"github.com/rebel-l/sessionservice/src/request"
 )
 
 func TestEndpointSessionPutNewPut(t *testing.T) {
@@ -117,11 +119,93 @@ func TestEndpointSessionPutLoadDataUnhappy(t *testing.T) {
 	storage.AssertExpectations(t)
 }
 
+func TestEndpointSessionPutGetRequestBodyHappy(t *testing.T) {
+	// setup
+	requestBodyRaw, err := http.NewRequest("POST", "/session", strings.NewReader(`{"id": "myId","data":{"key":"value"}}`))
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	storage := new(storage.HandlerMock)
+	session := getSessionMock(storage)
+	put := NewPut(session)
+
+	// do the test
+	body, err := put.getRequestBody(requestBodyRaw)
+	assert.Nil(t, err, "getRequestBody should not cause an error")
+	assert.Equal(t, "myId", body.Id, "Id was not read correct")
+	assert.Equal(t, "value", body.Data["key"], "Data was parsed wrong")
+}
+
+func TestEndpointSessionPutGetRequestUnhappy(t *testing.T) {
+	// setup
+	requestBodyRaw, err := http.NewRequest("POST", "/session", strings.NewReader("not a JSON"))
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	storage := new(storage.HandlerMock)
+	session := getSessionMock(storage)
+	put := NewPut(session)
+
+	// do the test
+	body, err := put.getRequestBody(requestBodyRaw)
+	assert.True(t, strings.Contains(err.Error(), "Unable to read request body:"),"getRequestBody should cause an error")
+	assert.Empty(t, body.Id)
+	assert.Empty(t, body.Data)
+}
+
+func TestEndpointSessionPutValidateRequestBodyHappy(t *testing.T) {
+	// setup
+	body := make([]request.Update, 2)
+	body[0].Data = make(map[string]string)
+	body[0].Data["key"] = "value"
+
+	body[1].Id = "8d9af075-1aa6-46c0-913d-ff42f22ca307"
+	body[1].Data = body[0].Data
+
+	storage := new(storage.HandlerMock)
+	session := getSessionMock(storage)
+	put := NewPut(session)
+
+	// do the test
+	for _, v := range body {
+		assert.Nil(t, put.validateRequestBody(&v))
+	}
+}
+
+func TestEndpointSessionPutValidateRequestBodyUnappy(t *testing.T) {
+	// setup
+	cases := make([]dataProviderValidateUnhappy, 2)
+
+	// case 1: empty id, empty data ==> first object is empty
+	cases[0].body = new(request.Update)
+	cases[0].err = "request body validation failed ==> no data field received"
+
+	// case 2: wrong formated UUID
+	cases[1].body = new(request.Update)
+	cases[1].body.Id = "wrong format of id"
+	cases[1].err = "request body validation failed ==> wrong UUID provided"
+
+	storage := new(storage.HandlerMock)
+	session := getSessionMock(storage)
+	put := NewPut(session)
+
+	// do the test
+	for _, v := range cases {
+		err := put.validateRequestBody(v.body)
+		assert.Equal(t, v.err, err.Error(), "Wrong error returned")
+	}
+}
+
+type dataProviderValidateUnhappy struct {
+	body *request.Update
+	err string
+}
+
 /**
 ToDo: missing tests ...
 	Handler()
-	getRequestBody()
-	validateRequestBody()
 	mergeData()
  */
 
